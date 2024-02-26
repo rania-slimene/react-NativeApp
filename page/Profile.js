e1c6e1a989aa815121d8d4026370da678ff42ec1
rania-slimene <raniaslimeneiset@gmail.com>
2024-02-26 20:18:58 +0100

    commit redux v2

--- page/Profile.js
+++ page/Profile.js
@@ -1,20 +1,58 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useDispatch , useSelector} from 'react-redux';
+import { StyleSheet, Text, View , Switch}  from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {toggleDark} from '../DarkModeSlice'
 export default function App() {
+
+  const isDarkModeEnabled = useSelector((state) => state.DarkMode.isDarkModeEnabled);
+  const dispatch = useDispatch();
+  const toggleMode = () => {
+    dispatch(toggleDark());
+  };
   const Stack = createNativeStackNavigator();
   return (
-<View style={styles.container}>
-    <Text>Hello Profile  page  </Text>
-</View>
+    <>
+    
+    <View style={isDarkModeEnabled ? styles.containerDark : styles.container}>
+    <Switch  style={styles.Switch} value={isDarkModeEnabled} onValueChange={toggleMode} />
+      <Text style={isDarkModeEnabled ? styles.textDark:styles.textLight}>
+        {isDarkModeEnabled ? 'Dark Mode Enabled' : 'Light Mode Enabled'}
+      </Text>
+      
+      <Text style={isDarkModeEnabled ? styles.textDark:styles.textLight}>Hello Profile  page  </Text>
+    </View>
+
+</>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    position: 'relative',
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  Switch: {
+   position : "absolute",
+   top: 5,
+   right: 5
+  
+  },
+
+  containerDark: {
+    flex: 1,
+    backgroundColor: "black",
+    alignItems: 'center',
+    justifyContent: 'center',
+   
+  },
+   textDark:{
+    color: "white"
+  },
+  textLight:{
+color: "black"
+  }
 });
